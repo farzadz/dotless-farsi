db9502c63927b121d15ecaf2479e96dd97c500ee
Farzad <farzad.vazir@gmail.com>
2020-02-14 18:12:42 +1100

    Add translation script

---
+++ dotless.py
@@ -0,0 +1,16 @@
+alpha = u'ءأئؤآابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی'
+dotlessalpha = u'ءأئؤآاٮٮٮٮححححددرررسسصصططععڡٯکگلمںوهی'
+mapping = dict(zip(alpha, dotlessalpha))
+
+def translate(text):
+    result = []
+    for i, letter in enumerate(text):
+        if letter in mapping:
+            # handling yeh character (ی)
+            if ord(letter) == 1740 and not (i == len(text)-1 or text[i+1] == ' '):
+                result.append(chr(1646))
+            else:
+                result.append(mapping[letter])
+        else:
+            result.append(letter)
+    return ''.join(result)
